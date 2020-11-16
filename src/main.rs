adf8a31856a50f811e6ae3031b180f0b7460220e
50ShadesOfCode <kaktysh20040130@gmail.com>
2020-11-16 18:29:29 +0300

    added clap support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate clap;
+use clap::{crate_version, App};
+
 fn main() {
+    let _matches = App::new("kami")
+        .version(crate_version!())
+        .about("kami app")
+        .author("50ShadesOfCode <killershadow425@gmail.com>")
+        .get_matches();
     println!("Hello, world!");
 }
